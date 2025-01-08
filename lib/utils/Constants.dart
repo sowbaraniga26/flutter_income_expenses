83b7d74be9b7c747455b8e534a42d8d78ffec0b6
sowbaraniga26 <sowbaraniga26@gmail.com>
2025-01-08 19:01:03 +0530

    ALL, INCOME, EXPENSE route prefix v2 added

---
+++ lib/utils/Constants.dart
@@ -0,0 +1,11 @@
+class Constants  {
+
+  static const String BASE_URL = "http://127.0.0.1:8000/api";
+
+  static const String ALL_TRANSACTION_ROUTE = "/v2/transactions";
+
+  static const String INCOME_TRANSACTION_ROUTE = "/v2/transactions?type=income";
+
+  static const String EXPENSE_TRANSACTION_ROUTE = "/v2/transactions?type=expense";
+
+}
