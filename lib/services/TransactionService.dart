add86b7f4bd5f6844e91f4c70bcf065dcf6e2e92
sowbaraniga26 <sowbaraniga26@gmail.com>
2025-01-18 17:25:26 +0530

    Create TransactionServiice added

---
+++ lib/services/TransactionService.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:flutter_income_expenses/utils/Constants.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'utils/Constants.dart';
+
+class TransactionService {
+
+  Future<void> createTransaction({
+    required String type,
+
+    required double amount,
+
+    required String description,
+
+    required String date,
+  }) async {
+
+    final url = Uri.parse(Constants.BASE_URL + Constants.ADD_TRANSACTION);
+
+    final headers = {'Content-Type': 'application/json'};
+
+    final body = jsonEncode({
+      'type': type,
+      'amount': amount,
+      'description': description,
+      'date': date,
+    });
+
+    try {
+
+      final response = await http.post(url, headers: headers, body: body);
+      if (response.statusCode == 201) {
+        // Success
+        print("Transaction created successfully");
+      } else {
+        // Error handling
+        print("Failed to create transaction: ${response.body}");
+
+        print(Constants.BASE_URL + Constants.ADD_TRANSACTION);
+      }
+    } catch (error) {
+      print("Error occurred: $error");
+    }
+  }
+}
